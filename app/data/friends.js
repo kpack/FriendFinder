16968330171665f48a198bae9b117409eeed0018
kpack <kevinpackler@gmail.com>
2017-08-19 06:42:25 -0400

    change

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+   {
+      "name":"Billy Bones",
+      "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Squirtle-Pokemon-Go.png",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"Carla Conondrum",
+      "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Cleffa-Pokemon-Go.png",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+   {
+      "name":"Silky Slim",
+      "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Charmander-Pokemon-Go.png",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+   {
+      "name":"Jennifer Jewels",
+      "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Magby-Pokemon-Go.png",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+	{
+	   "name":"Laura La One",
+	   "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Smoochum-Pokemon-Go.png",
+	   "scores":[4,3,4,1,5,2,5,3,1,4]
+	},
+      {
+      "name":"Henry Hopper",
+      "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Venusaur-Pokemon-Go.png",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+	{
+	   "name":"Chris Chopper",
+	   "photo":"https://rankedboost.com/wp-content/plugins/ice/pokemon-go/Pichu-Pokemon-Go.png",
+	   "scores":[4,4,2,3,2,2,3,2,4,5]
+	}
+
+]
+
+module.exports = friends;
